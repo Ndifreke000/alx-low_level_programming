6245c2e65601c49ac97b19576a543b88447f2a1c
Ndifreke000 <111875002+Ndifreke000@users.noreply.github.com>
2022-09-20 15:03:17 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,24 @@
-over
+/*
+ * File: 4-print_rev.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+/**
+* print_rev - prints reversed string, followed by a new line
+* @s: pointer to the string to print
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
