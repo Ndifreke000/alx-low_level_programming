b694b50de4a16a9c8122c55176165b2f44231db4
Ndifreke000 <111875002+Ndifreke000@users.noreply.github.com>
2022-09-29 00:05:52 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,24 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * @s: pointer to the string
+ * Return: void
  */
+
+
 void _puts_recursion(char *s)
 {
+
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
 }
+Footer
+© 2022 GitHub, Inc.
