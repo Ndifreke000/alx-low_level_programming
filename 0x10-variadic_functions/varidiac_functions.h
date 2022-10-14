3eeff671a348fb90c205b45cd3162322a0adfa31
Ndifreke000 <111875002+Ndifreke000@users.noreply.github.com>
2022-10-14 12:27:25 +0200

    Update varidiac_functions.h

--- 0x10-variadic_functions/varidiac_functions.h
+++ 0x10-variadic_functions/varidiac_functions.h
@@ -1,12 +1,9 @@
-#ifndef VAR_FUNC_H
-#define VAR_FUNC_H
-
-#include <stdio.h>
-#include <stdarg.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif
+#endif /* VARIADIC_FUNCTIONS_H*/
